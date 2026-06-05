caf06de41f72dc6d421a8aa1674f1ff97e85e41b
AnushkaJamkar <anushkajamkar@gmail.com>
2026-06-05 08:23:28 +0530

    Updated Neon database connection

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'postgres',
-  database: 'speedometer_db',
-  password: 'anushka',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
